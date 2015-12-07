ed729094f89a9673eb4805088bda0fea0049999f
Sandesh Bhusal <sandeshbhusal@outlook.com>
2015-12-07 19:47:42 +0545

    Trying to add support for geolocation
    Vai , js/listbuses.js ma geolocation le lyang garya xa. you sync garne
    bela samma problem xa. see it.

--- js/listbuses.js
+++ js/listbuses.js
@@ -1,4 +1,11 @@
 if(Parse){
+    var userLat = 0;
+    var userLng = 0;
+    var geo = navigator.geoLocation.getCurrentPosition(setPosition);
+    function setPosition(geoData){
+        userLat = geoData.coords.latitude;
+        userLng = geoData.coords.longitude;
+    }
     var vehicles = Parse.Object.extend("vehicle");
     var vehicle = new vehicles();
     var query = new Parse.Query(vehicle);
@@ -10,9 +17,18 @@ if(Parse){
             var re = result.get("RouteEnd");
             var st = result.get("Jammed");
             
+            var from = new google.maps.LatLng(27.700769, 85.300140);
+            var to   = new google.maps.LatLng(27.6833333, 84.4333333);
+            var dist = google.maps.geometry.spherical.computeDistanceBetween(from, to);
+            dist = dist / 1000;
+            dist = Math.floor(dist);
+            console.log(dist);
+            
+            
             console.log(result);
             $("#sp_id").html(sp);
             $("#sp_route").html("Runs "+rs+" to "+re+"<br />");
+            $("#distance").html(dist+" km far from You.");
         },
         error: function(Object, errorCode){
             console.log(errorCode);
